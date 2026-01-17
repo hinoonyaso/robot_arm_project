5ba6bef5ac320c3c2a219602bf71f08789c799cc
sang <hinoonyaso@gmail.com>
2026-01-17 20:19:28 +0900

    Declare OMPL planning pipeline

--- robot_arm_pick_place/colcon_ws/src/arm_moveit_config/launch/move_group.launch.py
+++ robot_arm_pick_place/colcon_ws/src/arm_moveit_config/launch/move_group.launch.py
@@ -41,41 +41,37 @@ def generate_launch_description():
     ompl_planning_yaml = load_yaml(os.path.join(moveit_share, "config", "ompl_planning.yaml"))
     moveit_controllers = load_yaml(os.path.join(moveit_share, "config", "controllers.yaml"))
 
+    planning_pipelines_config = {
+        "planning_pipelines": ["ompl"],
+        "default_planning_pipeline": "ompl",
+        "ompl": {},
+    }
+
+    if ompl_planning_yaml and "move_group" in ompl_planning_yaml and "ros__parameters" in ompl_planning_yaml["move_group"]:
+        planning_pipelines_config["ompl"] = ompl_planning_yaml["move_group"]["ros__parameters"]
+
     planning_parameters = {
         "use_sim_time": True,
         "moveit_controller_manager": "moveit_simple_controller_manager/MoveItSimpleControllerManager",
+        "moveit_manage_controllers": True,
         "planning_scene_monitor": {
             "publish_planning_scene": True,
             "publish_geometry_updates": True,
             "publish_state_updates": True,
             "publish_transforms_updates": True,
         },
-        "planning_plugin": "ompl_interface/OMPLPlanner",
-        "request_adapters": (
-            "default_planner_request_adapters/AddTimeOptimalParameterization "
-            "default_planner_request_adapters/ResolveConstraintFrames "
-            "default_planner_request_adapters/FixWorkspaceBounds "
-            "default_planner_request_adapters/FixStartStateBounds"
-        ),
-        "start_state_max_bounds_error": 0.1,
+        "trajectory_execution.allowed_execution_duration_scaling": 1.2,
+        "trajectory_execution.allowed_goal_duration_margin": 0.5,
+        "trajectory_execution.allowed_start_tolerance": 0.01,
     }
 
     merge_config(planning_parameters, kinematics_yaml)
     merge_config(planning_parameters, joint_limits_yaml)
-    merge_config(planning_parameters, ompl_planning_yaml)
     merge_config(planning_parameters, moveit_controllers)
 
+    planning_parameters.update(planning_pipelines_config)
     planning_parameters.update(robot_description)
     planning_parameters.update(robot_description_semantic)
-
-    planning_parameters.update(
-        {
-            "moveit_manage_controllers": True,
-            "trajectory_execution.allowed_execution_duration_scaling": 1.2,
-            "trajectory_execution.allowed_goal_duration_margin": 0.5,
-            "trajectory_execution.allowed_start_tolerance": 0.01,
-        }
-    )
 
     move_group_node = Node(
         package="moveit_ros_move_group",
